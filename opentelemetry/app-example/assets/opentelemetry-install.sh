7c2c2e747af64638b02fcc491b2b133763410530
Wanderson Camargo <wandersonwhcr@gmail.com>
2022-09-24 09:05:32 -0300

    Retrieve SDK using init container

--- opentelemetry/app-example/assets/opentelemetry-install.sh
+++ opentelemetry/app-example/assets/opentelemetry-install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 set -xe
 trap exit 1 2 15
+apt-get update
+apt-get install wget
 cd /opt
 wget https://github.com/open-telemetry/opentelemetry-cpp-contrib/releases/download/webserver%2Fv1.0.1/opentelemetry-webserver-sdk-x64-linux.tgz
-ls -alsh
+tar xvzf opentelemetry-webserver-sdk-x64-linux.tgz
+find ./opentelemetry-webserver-sdk
